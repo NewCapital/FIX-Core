b6accc5835bcce633bc6d6742dd06cf8fe748c24
Kestutis Gudynas <44440041+kemi04@users.noreply.github.com>
2019-08-20 14:55:53 +0100

    Update masternode.h

--- src/masternode.h
+++ src/masternode.h
@@ -31,7 +31,7 @@ class CMasternodePing;
 extern map<int64_t, uint256> mapCacheBlockHashes;
 
 bool GetBlockHash(uint256& hash, int nBlockHeight);
-static int GetMasternodeTierRounds(CTxIn vin)
+static int GetMasternodeTierRounds(CTxIn vin);
 
 //
 // The Masternode Ping Class : Contains a different serialize method for sending pings from masternodes throughout the network
