985f0abf70e08c899e2e64e0db6cb8c0beb941ed
yurykk <yurykk@gmail.com>
2020-02-01 12:30:38 +0200

    [Liquid369] In this instance when no MN payment it would make this vout negative if I = 1 with only one output.

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -389,7 +389,7 @@ void CMasternodePayments::FillBlockPayee(CMutableTransaction& txNew, int64_t nFe
 
             //subtract mn payment from the stake reward
             if (!txNew.vout[1].IsZerocoinMint())
-                txNew.vout[i - 1].nValue -= nDevReward;
+                txNew.vout[i].nValue -= nDevReward;
         }
     }
 }
