cec7a16ba58d277ec08dfa0812d53a28183d47ef
yurykk <yurykk@gmail.com>
2020-03-21 13:21:51 +0200

    [Core] Min Staking Input - Fix nStakeMinInput - Fix spork definitions - change minStakeInput starting blocks
    # Conflicts:
    #	src/main.cpp
    #	src/spork.cpp
    #	src/spork.h

--- src/main.cpp
+++ src/main.cpp
@@ -3937,7 +3937,8 @@ bool CheckBlock(const CBlock& block, CValidationState& state, bool fCheckPOW, bo
         CBlockIndex* pindex = chainActive.Tip();
         int nHeight = pindex->nHeight;
         // Ensure the output of the stake is above min amount (20000 for TWINS)
-        if (IsSporkActive(SPORK_FIX_02_MIN_STAKE_AMT && nHeight >= 300000)) {
+        if (IsSporkActive(SPORK_FIX_02_MIN_STAKE_AMOUNT &&
+              nHeight >= (Params().NetworkID() == CBaseChainParams::MAIN? 215600 : 220000) )) {
 
             if (block.vtx[1].vout[1].nValue < Params().StakingMinInput())
                 return state.DoS(100, error("CheckBlock() : stake under min. stake value"));
